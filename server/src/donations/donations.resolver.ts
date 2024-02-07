9212a9d2782b9113f6bc3c20402d16f28ead6c40
Dmytro Onufrienko <120226231+Dmytro-Onufrienko@users.noreply.github.com>
2024-02-07 15:20:59 +0200

    .

--- server/src/donations/donations.resolver.ts
+++ server/src/donations/donations.resolver.ts
@@ -1,15 +1,29 @@
-import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
+import { Resolver, Query, Mutation, Args, Subscription } from '@nestjs/graphql';
 import { DonationsService } from './donations.service';
 import { OrderByParams } from 'src/graphql';
 import { DonationCreateInput } from 'src/@generated/prisma-nestjs-graphql/donation/donation-create.input';
+import { PubSub } from 'graphql-subscriptions';
+
+const publicSubscription = new PubSub();
 
 @Resolver('Donation')
 export class DonationsResolver {
   constructor(private readonly donationsService: DonationsService) {}
 
   @Mutation('createDonation')
-  create(@Args('createDonationInput') createDonationInput: DonationCreateInput) {
-    return this.donationsService.create(createDonationInput);
+  async create(
+    @Args('createDonationInput') createDonationInput: DonationCreateInput,
+  ) {
+    const newDonation = await this.donationsService.create(createDonationInput);
+    const total = await this.donationsService.getTotal();
+    publicSubscription.publish('totalUpdated', { totalUpdated: { total } });
+
+    return newDonation;
+  }
+
+  @Subscription()
+  totalUpdated() {
+    return publicSubscription.asyncIterator('totalUpdated');
   }
 
   @Query('donations')
